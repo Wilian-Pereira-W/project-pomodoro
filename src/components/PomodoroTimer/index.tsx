d75b9a9221adb694c8a875a9b9ad78a730456747
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-02 13:56:02 -0300

    feat: implementado os audios

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -3,6 +3,14 @@ import { useInterval } from '../../hooks/useInterval';
 import Button from '../Button';
 import Timer from '../Timer';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../../sounds/bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../../sounds/bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
+
 interface Props {
   pomodoroTime: number;
   shortRestTime: number;
@@ -32,6 +40,7 @@ function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true);
     setResting(false);
     setMainTime(props.pomodoroTime);
+    audioStartWorking.play();
   };
 
   const configureRest = (long: boolean) => {
@@ -44,6 +53,7 @@ function PomodoroTimer(props: Props): JSX.Element {
     } else {
       setMainTime(props.shortRestTime);
     }
+    audioStopWorking.play();
   };
 
   return (
