b1321ff8e274461975f9a961f764d5b11b9ec2b9
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-02 10:46:56 -0300

    feat: implementado o componente Timer

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,10 @@
+import { secondsToTime } from '../utils/secondsToTime';
+
+interface Props {
+  mainTimer: number;
+}
+function Timer(props: Props): JSX.Element {
+  return <div className="timer">{secondsToTime(props.mainTimer)}</div>;
+}
+
+export default Timer;
