b43dc5202f77feff98f6966a311b0dc5f29834cb
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-02 19:20:05 -0300

    feat: implementado disabled do button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,10 +2,16 @@ interface Props {
   text: string;
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
 }
 function Button(props: Props): JSX.Element {
   return (
-    <button onClick={props.onClick} className={props.className} type="button">
+    <button
+      onClick={props.onClick}
+      className={props.className}
+      disabled={props.disabled}
+      type="button"
+    >
       {props.text}
     </button>
   );
