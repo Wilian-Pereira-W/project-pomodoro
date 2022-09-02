09c9ea206cbfd1281a2ed1810a12894f8b1adf92
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-02 19:19:32 -0300

    feat: implementado o pomodoroOption

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
 import PomodoroTimer from './components/PomodoroTimer';
 
 function App(): JSX.Element {
+  const [pomodoroTime] = useState<number>(25);
+  const [shortBreakTime] = useState<number>(5);
+  const [longBreakTime] = useState<number>(15);
+
   return (
     <div className="container">
+      <form className="pomodoroOption">
+        <label htmlFor="pomodoro">
+          Pomodoro
+          <input
+            type="button"
+            name="pomodoro"
+            id="pomodoro"
+            value={`${pomodoroTime}:00`}
+          />
+        </label>
+        <label htmlFor="Intervalo-curto">
+          Short Break
+          <input
+            type="button"
+            name="Intervalo-curto"
+            id="Intervalo-curto"
+            value={`0${shortBreakTime}:00`}
+          />
+        </label>
+        <label htmlFor="Intervalo-longo">
+          Long Break
+          <input
+            type="button"
+            name="Intervalo-longo"
+            id="Intervalo-logo"
+            value={`${longBreakTime}:00`}
+          />
+        </label>
+      </form>
       <PomodoroTimer
-        pomodoroTime={1500}
-        shortRestTime={300}
-        longRestTime={900}
+        pomodoroTime={pomodoroTime * 60}
+        shortRestTime={shortBreakTime * 60}
+        longRestTime={longBreakTime * 60}
         cycles={4}
+        disabled={pomodoroTime <= 0}
       />
     </div>
   );
